ec1244131b6e6efa23c677b3b5ba4c7fc0b7f420
wkwksama <mailpakgasing@gmail.com>
2018-07-09 11:45:43 +0700

    sudah pakai agendash

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 console.log('asw');
 const mongoString = process.env.MONGO_STRING || 'mongodb://127.0.0.1/example'
 
+const express = require('express')
+const app = express()
+
 const mongojs = require('mongojs')
 const signale = require('signale');
 const Agenda = require('agenda');
+var Agendash = require('agendash');
 
 const db = mongojs(mongoString, ['emailFls17'])
 
 const agenda = new Agenda({ db: { address: mongoString } });
 // agenda.processEvery('3 minutes')
-agenda.processEvery('10 seconds')
+agenda.processEvery('20 seconds')
 agenda.maxConcurrency(5);
 agenda.defaultLockLimit(5);
 
-let i = 0
+let dashMiddleware = (req, res, next) => {
+  if (req.params.pass == 'wkwksama') {
+    next()
+  } else {
+    res.send('dilarang masuk')
+  }
+}
+
+app.use('/dash/:pass', dashMiddleware, Agendash(agenda));
+app.get('/tes', (req, res) => {
+  res.json('Hello World!')
+})
 
 db.emailFls17.find({
   bio_email: { $nin: [null] }
 }).forEach(function(err, doc) {
   if (err) return console.log('err', err);
   if (!doc) return
-  // setTimeout(() => {
-  //   console.log('>> ', doc._id, i++);
-  // }, Math.floor(Math.random() * 10000))
   signale.watch('>> email masuk schedule', doc.bio_email)
   require('./utils/schedule-email')(agenda, db)
   agenda.start()
@@ -30,4 +42,5 @@ db.emailFls17.find({
     email: doc.bio_email,
   });
 });
-console.log('<<<<<<< selesai', i);
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
